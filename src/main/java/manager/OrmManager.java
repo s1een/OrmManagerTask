ac1dfd43f6fd15dd942abdc3aff4f60f8354c8b0
nikovlad <vladislav.nikolenko@ukr.net>
2022-02-02 23:53:35 +0200

    KH583ORM1-8 : implemented - where, delete.

--- src/main/java/manager/OrmManager.java
+++ src/main/java/manager/OrmManager.java
@@ -9,7 +9,7 @@ import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.StringJoiner;
+import java.util.*;
 
 public class OrmManager {
     Connection connection;
@@ -89,5 +89,82 @@ public class OrmManager {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+    public void delete(Class<?> table, String condition) {
+        Entity entity = table.getAnnotation(Entity.class);
+        List<String> fields = getFields(table);
+        if (where(condition,fields))
+        {
+            var cmd = "DELETE FROM " + entity.value() + " WHERE " + condition.toUpperCase() + ";";
+            runCommand(cmd);
+        }
+        else System.out.println("Wrong condition format");
+
+    }
+
+    private boolean where(String condition, List<String> fields){
+        return checkCondition(condition,fields);
+    }
+
+    private boolean checkCondition(String condition, List<String> fields) {
+        var conditionSymbol = checkConditionSymbol(condition);
+        if(conditionSymbol == "-1")
+            return false;
+        var position = condition.lastIndexOf(conditionSymbol);
+        var columnName= condition.substring(0,position).toUpperCase();
+        for (String f : fields) {
+            if(Objects.equals(f.toString().toUpperCase() ,columnName))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String checkConditionSymbol(String condition){
+        String[] symbols = {};
+        symbols = new String[]{"=",">",">=","<","<=","<>"," LIKE ", " IN "};
+        for (String s : symbols)
+            if(condition.lastIndexOf(s) != -1)
+                return s;
+        return "-1";
+    }
+
+    private List<String> getFields(Class<?> table){
+        Entity entity = table.getAnnotation(Entity.class);
+        List<String> fields = new ArrayList<>();
+        if (entity == null) {
+            System.out.println("No table annotations for class " + table.getName());
+        } else {
+            for (Field field : table.getDeclaredFields()) {
+                Annotation[] annotations = field.getDeclaredAnnotations();
+
+                for (var annotation:annotations) {
+                    if (annotation instanceof Id) {
+                        fields.add(readAnnotation(field,(Id)annotation));
+                    }
+                    if(annotation instanceof Column) {
+                        fields.add(readAnnotationColumn(field,(Column)annotation));
+                    }
+                }
+            }
+        }
+        return fields;
+    }
+
+    private String readAnnotation(Field field, Id id) {
+
+        return (Objects.equals((id).value(), "") ? field.getName() : id.value()).toUpperCase();
+    }
+
+    private String readAnnotationColumn(Field field, Column column) {
+        return (Objects.equals((column).value(), "") ? field.getName() : column.value()).toUpperCase();
+    }
+
+    private static String removeLastChar(String s) {
+        return Optional.ofNullable(s)
+                .filter(str -> str.length() != 0)
+                .map(str -> str.substring(0, str.length() - 1))
+                .orElse(s);
     }
 }
